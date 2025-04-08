1557ffb68d90f871dfcd45ff76700d299a400ba5
LeeKM321 <stephen4951@gmail.com>
2025-04-08 17:22:37 +0900

    상태 끌어올리기 (props drilling)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Hello from './components/Example';
 import Greet from './components/Greet';
@@ -13,11 +13,25 @@ function App() {
     { id: 2, title: '양파', price: 5000, date: new Date(2023, 6, 20) },
   ];
 
+  // 지출 객체 배열을 상태변수로 관리 (변화가 생기면 재 렌더링 대상으로 삼기 위해)
+  const [expenseList, setExpenseList] = useState(expenses);
+
+  // 자식 컴포넌트의 데이터를 부모 컴포넌트에서 받아내는 방법. (props drilling)
+  const addExpenseHandler = (newEx) => {
+    const modifyEx = {
+      ...newEx,
+      id: expenseList[expenseList.length - 1].id + 1,
+    };
+    // push로 추가하면 리액트가 몰라요!!! setter로 변경하셔야 합니다!!
+    // 스프레드 문법을 활용하여 expenseList 그대로 유지하면서 modifyEx만 추가
+    setExpenseList([...expenseList, modifyEx]);
+  };
+
   return (
     <>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <div className='expenses'>
-        {expenses.map((item) => (
+        {expenseList.map((item) => (
           <ExpenseItem
             key={item.id} // 반복문을 통해 같은 컴포넌트를 표현할 때, 각각을 구분할 수 있게 해 주는 props
             title={item.title}
