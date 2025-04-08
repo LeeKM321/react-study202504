5793b70ed2d8bf2a915618c7366d5045b3455a0b
LeeKM321 <stephen4951@gmail.com>
2025-04-08 15:26:39 +0900

    지출내역 입력창 제공

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import Greet from './components/Greet';
 import Bye from './components/Bye';
 import ExpenseItem from './components/expenses/ExpenseItem';
 import NoName from './NoName';
+import NewExpense from './components/newExpense/NewExpense';
 
 function App() {
   const expenses = [
     { id: 1, title: '냠냠치킨', price: 19000, date: new Date(2023, 6, 19) },
     { id: 2, title: '양파', price: 5000, date: new Date(2023, 6, 20) },
-    { id: 3, title: '포도', price: 20000, date: new Date(2023, 6, 21) },
-    { id: 4, title: '오렌지', price: 15000, date: new Date(2023, 6, 22) },
   ];
 
   return (
     <>
+      <NewExpense />
       <div className='expenses'>
         {expenses.map((item) => (
           <ExpenseItem
