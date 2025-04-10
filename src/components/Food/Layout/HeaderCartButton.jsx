af042d977fbf3106d83c352aaa534d29ee773929
LeeKM321 <stephen4951@gmail.com>
2025-04-10 16:38:54 +0900

    Food 템플릿 구성 중 (Header Button)

---
+++ src/components/Food/Layout/HeaderCartButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './HeaderCartButton.module.scss';
+import CartIcon from '../Cart/CartIcon';
+
+const HeaderCartButton = () => {
+  // css module에서 가져온 클래스들을 디스트럭쳐링 해서 사용.
+  const { button, icon, badge } = styles;
+
+  return (
+    <button className={button}>
+      <span className={icon}>
+        <CartIcon />
+      </span>
+      <span>My Cart</span>
+      <span className={badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
