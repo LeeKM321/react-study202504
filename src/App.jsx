ece767d876f56c08c089748da1bee141a58e448f
LeeKM321 <stephen4951@gmail.com>
2025-04-10 11:49:46 +0900

    useEffect 기초

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Login from './components/SideEffect/Login/Login';
 import Home from './components/SideEffect/Home/Home';
@@ -25,12 +25,20 @@ const App = () => {
     localStorage.removeItem('login-flag');
   };
 
-  // 화면이 렌더링 될 때 localStorage를 확인해서
-  // login-flag라는 데이터 있다면 로그인 상태를 변경하자
-  const storedLoginFlag = localStorage.getItem('login-flag');
-  if (storedLoginFlag === '1') {
-    setIsLoggedIn(true);
-  }
+  useEffect(() => {
+    // 이 useEffect의 콜백은 최초 렌더링 시 딱 한번만 동작해도 되는 로직.
+    console.log('useEffect Called!');
+
+    // 화면이 렌더링 될 때 localStorage를 확인해서
+    // login-flag라는 데이터 있다면 로그인 상태를 변경하자
+    const storedLoginFlag = localStorage.getItem('login-flag');
+    if (storedLoginFlag === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // 의존성 배열: useEffect가 실행되어야 하는 트리거 변수.
+  // 배열 안에 상태 변수를 지정하면, 해당 변수의 값이 변할 때마다 useEffect가 실행되고,
+  // 만약 빈 배열을 전달하면 최초 렌더링 과정에서 단 한번만 실행.
 
   return (
     <>
