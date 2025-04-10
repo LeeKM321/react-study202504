454065e4310560bfbefe2d1f852b3e3b19cb60e2
LeeKM321 <stephen4951@gmail.com>
2025-04-10 17:38:18 +0900

    Cart Modal 열고 닫기 처리

--- src/components/Food/Layout/HeaderCartButton.jsx
+++ src/components/Food/Layout/HeaderCartButton.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from './HeaderCartButton.module.scss';
 import CartIcon from '../Cart/CartIcon';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = ({ onShow }) => {
   // css module에서 가져온 클래스들을 디스트럭쳐링 해서 사용.
   const { button, icon, badge } = styles;
 
   return (
-    <button className={button}>
+    <button className={button} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
