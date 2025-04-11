e3ac897a99dad88c4a2d5c71c397b3a094e83cae
LeeKM321 <stephen4951@gmail.com>
2025-04-11 10:26:13 +0900

    Cart에 Item 담기 기능, Modal 렌더링, Badge 수량 렌더링

--- src/components/Food/Layout/HeaderCartButton.jsx
+++ src/components/Food/Layout/HeaderCartButton.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './HeaderCartButton.module.scss';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../store/cart-context';
 
 const HeaderCartButton = ({ onShow }) => {
   // css module에서 가져온 클래스들을 디스트럭쳐링 해서 사용.
   const { button, icon, badge } = styles;
+
+  const { items } = useContext(CartContext);
+
+  const numberOfCart = items.reduce((accu, item) => accu + item.amount, 0);
 
   return (
     <button className={button} onClick={onShow}>
@@ -12,7 +17,7 @@ const HeaderCartButton = ({ onShow }) => {
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>3</span>
+      <span className={badge}>{numberOfCart}</span>
     </button>
   );
 };
